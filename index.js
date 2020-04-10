51a6da5fe83178558580d57126c52758a5c38b6b
Jesus <michel.js66@gmail.com>
2020-04-09 21:16:13 -0300

    adicionado middleware de contagem de requisições e middleware que checa se um projeto existe ou não.

--- index.js
+++ index.js
@@ -10,12 +10,33 @@ const projects = [ {
     tasks: ["Nova tarefa"]
   }];
 
+function requestCount(req, res, next) {
+    console.count('Requisições até o momento');
+    next();
+}
+
+function checkProjectExists(req, res, next) {
+    const { id } = req.params;
+
+    const project = projects.find(project => project.id === id);
+
+    if(!project){
+        res.status(400).json({error: 'Id dos not existis'})
+    }
+    return next();
+}
+
+server.use(requestCount);
+
 server.get('/projects', (req, res) => {
     return res.json(projects);
 })
 
-server.get('/projects/:index', (req, res) => {
-    const { index } = req.params;
+server.get('/projects/:id', checkProjectExists, (req, res) => {
+    const { id } = req.params;
+
+    const index = projects.findIndex(project => project.id === id);
+
     return res.json(projects[index]);
 })
 
@@ -24,13 +45,25 @@ server.post('/projects', (req, res) => {
 
     const { title } = req.body;
     
-    var project = {id: id, title: title};
+    var project = {id: id, title: title, tasks:[]};
     projects.push(project);
 
     return res.json({projects});
 })
 
-server.put('/projects/:id', (req, res) => {
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+    const { id } = req.params;
+
+    const { title } = req.body;
+
+    const index = projects.findIndex(project => project.id === id);
+
+    projects[index].tasks.push(title);
+
+    return res.json({projects});
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
 
@@ -41,7 +74,7 @@ server.put('/projects/:id', (req, res) => {
     return res.json({projects});
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
     const { id } = req.params;
 
     const index = projects.findIndex(project => project.id === id);
